d63a833f3ced633ba0ba185b8452f42f1dc21fdf
Ryan Boder <ryan.boder@mheducation.com>
2016-01-21 23:32:41 -0500

    Added coin slot sum test 2 nickels

--- src/test/java/CoinSlotTest.java
+++ src/test/java/CoinSlotTest.java
@@ -16,4 +16,10 @@ public class CoinSlotTest {
     assertEquals("0.05", out.calculateValue().toString());
   }
 
+  @Test public void testValueWhenTwoNickelsAdded() {
+    out.insertCoin(5.000, 21.21);
+    out.insertCoin(5.000, 21.21);
+    assertEquals("0.10", out.calculateValue().toString());
+  }
+
 }
