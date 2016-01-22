789b367bee1f9c7e7547d20336715190f6b80edc
Ryan Boder <ryan.boder@mheducation.com>
2016-01-21 22:23:52 -0500

    Added identify quarter

--- src/test/java/CoinTest.java
+++ src/test/java/CoinTest.java
@@ -8,8 +8,12 @@ public class CoinTest {
     assertEquals(Coin.DIME, Coin.identify(2.268, 17.91));
   }
 
-  @Test public void whenGivenNickelIdenitifyNickel() {
+  @Test public void whenGivenNickelIdentifyNickel() {
     assertEquals(Coin.NICKEL, Coin.identify(5.000, 21.21));
+  }
+
+  @Test public void whenGivenQuarterIdentifyQuarter() {
+    assertEquals(Coin.QUARTER, Coin.identify(5.670, 24.26));
   }
 
 }
