305445ce7b69732e127a1b12e65e8fd41150b018
Ryan Boder <ryan.boder@mheducation.com>
2016-01-21 22:18:56 -0500

    Added identify nickel

--- src/main/java/Coin.java
+++ src/main/java/Coin.java
@@ -1,7 +1,24 @@
+import org.apache.commons.math3.util.Precision;
+
+import java.util.Arrays;
+
 public enum Coin {
-  DIME, NICKEL, QUARTER;
+  //https://www.usmint.gov/about_the_mint/?action=coin_specifications
+  DIME(2.268, 17.91), NICKEL(5.000, 21.21), QUARTER(5.670, 24.26);
+
+  public final double weight;
+  public final double diameter;
+
+  Coin(double weight, double diameter) {
+    this.weight = weight;
+    this.diameter = diameter;
+  }
 
   public static Coin identify(double weight, double diameter) {
-    return DIME;
+    return Arrays.stream(Coin.values()).filter(x -> x.matches(weight, diameter)).findFirst().get();
+  }
+
+  private boolean matches(double weight, double diameter) {
+    return Precision.equals(this.weight, weight, 0.2) && Precision.equals(this.diameter, diameter, 0.5);
   }
 }
--- src/test/java/CoinTest.java
+++ src/test/java/CoinTest.java
@@ -7,4 +7,9 @@ public class CoinTest {
   @Test public void whenGivenDimeIdentifyDime() {
     assertEquals(Coin.DIME, Coin.identify(2.268, 17.91));
   }
+
+  @Test public void whenGivenNickelIdenitifyNickel() {
+    assertEquals(Coin.NICKEL, Coin.identify(5.000, 21.21));
+  }
+
 }
